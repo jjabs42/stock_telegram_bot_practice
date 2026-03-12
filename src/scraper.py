f14ee17a6a8d27ee8d9a848c5659a4c4836afb3b
Jacob Jabero <Jacob Jabero>
2026-03-11 22:34:33 -0400

    Basically implamented the scraper so now it gets the messages from the channels and looks for $

--- src/scraper.py
+++ src/scraper.py
@@ -9,19 +9,36 @@ envdir = Path(__file__).parent.parent
 load_dotenv(envdir/".env")
 api_id = os.getenv("api_id")
 api_hash = os.getenv("apihash")
-
-
+telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
 
 #Client Obj
 client = TelegramClient("Client1", api_hash=api_hash, api_id=api_id)
 
 #with basically connects and disconencts
-async def main():
+async def scrape(channel, lim):
     async with client:
-        await client.send_message("+12489618910", "hello sir")
+        #gets channel id
+        chatid = (await client.get_entity(channel)).id
+        
+        """
+        Gets messages with limit of last 100, then goes through each message object
+        and checks for a $, (message.message) where message 2 is an attribute of the message obj
+        """
+        messages = await client.get_messages(chatid, limit=lim)
+        for message in messages:
+            actual_message = message.message
+            if actual_message == None:
+                continue
+            for char in actual_message:
+                if char == "$":
+                    print(f"{actual_message}")
+
+async def main():
+    channel1 = "TopSecretStockAlerts"
+    await scrape(channel1, lim=200 )
         
     
 
 
-
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
